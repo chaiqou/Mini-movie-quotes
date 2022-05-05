a02dce4a86fd8886edd6a52ba99b2314890a4410
chaiqou <lomtadzenikusha@gmail.com>
2022-05-05 20:30:52 +0400

    feat: Create artisan command for adding new Admin

---
+++ app/Console/Commands/CreateAdminCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class CreateAdminCommand extends Command
+{
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'make:admin {--username=} {--email=} {--password=}';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Creates a new admin';
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return int
+	 */
+	public function handle()
+	{
+		$username = $this->option('username');
+		$email = $this->option('email');
+		$password = $this->option('password');
+
+		User::create([
+			'username'     => $username,
+			'email'        => $email,
+			'password'     => bcrypt($password),
+		]);
+
+		$this->info('Admin successfully created ^_^  ' . '  email:  ' . $email . '  username:  ' . $username . '  password:  ' . $password);
+	}
+}
